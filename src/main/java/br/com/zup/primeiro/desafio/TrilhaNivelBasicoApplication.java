a508f8d6be86fe6affc9eee92634ef7290a64410
eliasborgeszup <elias.borges@zup.com.br>
2021-01-06 09:02:08 -0300

    Criação de estrutura inicial do projeto

---
+++ src/main/java/br/com/zup/primeiro/desafio/TrilhaNivelBasicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.primeiro.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaNivelBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaNivelBasicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/primeiro/desafio/TrilhaNivelBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.primeiro.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilhaNivelBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
