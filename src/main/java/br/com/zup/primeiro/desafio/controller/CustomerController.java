07478051b117338277f88c41e5b8b00f2d183e85
eliasborgeszup <elias.borges@zup.com.br>
2021-01-08 18:24:40 -0300

    Annotations in CustomerController

--- src/main/java/br/com/zup/primeiro/desafio/controller/CustomerController.java
+++ src/main/java/br/com/zup/primeiro/desafio/controller/CustomerController.java
@@ -2,13 +2,17 @@ package br.com.zup.primeiro.desafio.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +33,7 @@ public class CustomerController {
 
 	@ResponseStatus(CREATED)
 	@PostMapping
-	public Long create(CustomerDTO costumerDTO) {
+	public Long create(@Valid @RequestBody CustomerDTO costumerDTO) {
 		return service.create(costumerDTO);
 	}
 
@@ -41,14 +45,14 @@ public class CustomerController {
 
 	@ResponseStatus(OK)
 	@GetMapping(value = "/{cpf}")
-	public Customer findByCpf(String cpf) {
+	public Customer findByCpf(@PathVariable String cpf) {
 		return service.findByCpf(cpf);
 	}
 
 	@ResponseStatus(OK)
 	@PutMapping(value = "/{cpf}")
-	public void update(CustomerDTO costumerDTO) {
-		service.update(costumerDTO);
+	public Long update(@Valid @RequestBody CustomerDTO costumerDTO) {
+		return service.update(costumerDTO);
 	}
 
 	@ResponseStatus(NO_CONTENT)
