80c32ec873e74b8506d3a1a11f518813ef472fa1
eliasborgeszup <elias.borges@zup.com.br>
2021-01-15 08:36:28 -0300

    Implements integration with marvel-api

---
+++ src/main/java/br/com/zup/primeiro/desafio/controller/MarvelController.java
@@ -0,0 +1,28 @@
+package br.com.zup.primeiro.desafio.controller;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zup.primeiro.desafio.controller.response.marvel.MarvelResponse;
+import br.com.zup.primeiro.desafio.service.MarvelComicsService;
+
+@RestController
+@RequestMapping(value = "/marvel")
+public class MarvelController implements MarvelComicsService{
+
+	MarvelComicsService service;
+
+	public MarvelController(MarvelComicsService service) {
+		this.service = service;
+	}
+
+	@ResponseStatus(OK)
+	@GetMapping(value = "/comics")
+	public MarvelResponse findAll() {
+		return service.findAll();
+	}
+}
---
+++ src/main/java/br/com/zup/primeiro/desafio/controller/response/marvel/DataResponse.java
@@ -0,0 +1,10 @@
+package br.com.zup.primeiro.desafio.controller.response.marvel;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class DataResponse {
+	private List<ResultsResponse> results;
+}
